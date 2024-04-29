6febb35fe8c321af7c0276c75196371dec4b3ba8
Joseph Erlinger <josepherlinger@dyn-10-109-37-193.wless.uoregon.edu>
2024-04-29 09:58:10 -0700

    Created Jump Feature

--- Assets/scripts/JumpFeature.cs
+++ Assets/scripts/JumpFeature.cs
@@ -1,18 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class JumpFeature : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private LineRenderer LineRenderer;
+    [Header("Display Controls Test")]
+    [SerializeField] 
+    [Range(10, 100)] 
+    private int LinePoints = 25;
+    [SerializeField]
+    [Range(0.01f, 0.25f)]
+    private float TimeBetweenPoints = 0.1f;
+    // private float gravity = -9.81f;
+    public Action<string> onJump;
 
-    // Update is called once per frame
-    void Update()
+    public void DrawProjection(Vector3 startPosition, Vector3 direction, float speed) 
     {
-        
+        LineRenderer.enabled = true;
+        LineRenderer.positionCount = Mathf.CeilToInt(LinePoints / TimeBetweenPoints) + 1;
+        Vector3 startVelocity = new Vector3(direction.x * speed, 10.0f, direction.y * speed);
+
+        // Vector3 startVelocity = new Vector3(velocityState.x * 2, 10.0f, velocityState.z * 2);
+        int i = 0;
+        LineRenderer.SetPosition(i, startPosition);
+        for (float time = 0; time < LinePoints; time += TimeBetweenPoints)
+        {
+            i++;
+            Vector3 point = startPosition + time * startVelocity;
+            // Explaination for equation below at timestamp 4:52.
+            point.y = startPosition.y + startVelocity.y * time + (Physics.gravity.y / 2f * time * time);
+            LineRenderer.SetPosition(i, point);
+        }   
+    }
+    public void Jump(Rigidbody entity, Vector3 direction, float speed)
+    {
+        onJump.Invoke("Started");
+        entity.velocity = new Vector3(direction.x * speed, 10f, direction.y * speed);
+        LineRenderer.enabled = false;
     }
 }
--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -7,16 +7,19 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(InputController))]
 public class Player_Controller : TankParent
 {
+    private JumpFeature _jumpFeature;
     private InputController _inputController;
+    private Rigidbody _rigidbody;
     private Vector3 velocity;
     private Vector2 keyData;
-    private bool isRotating, isDriving;
+    private bool isRotating, isDriving, moveEnabled;
 
     private void Awake()
     {
         keyData = new Vector2 (0, 0);
         isRotating = false;
         isDriving = false;
+        moveEnabled = true;
     }
 
     private void Start()
@@ -24,6 +27,9 @@ public class Player_Controller : TankParent
         _inputController = GetComponent<InputController>();
         _inputController.onWASD += HandleWASD;
         _inputController.onSpacebar += Jump;
+        _jumpFeature = GetComponent<JumpFeature>();
+        _jumpFeature.onJump += HandleJump;
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     private void HandleWASD(InputAction.CallbackContext ctx)
@@ -33,17 +39,54 @@ public class Player_Controller : TankParent
         isDriving = (keyData.y != 0) ? true : false;
     }
 
+    private void HandleJump(string msg)
+    {
+        if (msg == "Started")
+        {
+            moveEnabled = false;
+        }
+        if (msg == "Ended")
+        {
+            moveEnabled = true;
+        }
+    }
+
     private void Jump(InputAction.CallbackContext ctx)
     {
-        Debug.Log($"Spacebar! {ctx}");
+        InputActionPhase spacebarPhase = ctx.phase;
+        if (spacebarPhase == InputActionPhase.Started)
+        {
+            moveEnabled = false;
+            _jumpFeature.DrawProjection(transform.position, forward, speed);
+        }
+        if (spacebarPhase == InputActionPhase.Canceled)
+        {
+            moveEnabled = true;
+            _jumpFeature.Jump(_rigidbody, forward, speed);
+        }
+
+        Debug.Log($"Spacebar! {spacebarPhase}");
+    }
+
+    // Enable Moving after hitting the ground.
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Solid"))
+        {
+            moveEnabled = true;
+        }
     }
 
     private void FixedUpdate()
     {
+        if (!moveEnabled)
+            return; 
+
         if (isRotating)
         {
             Rotate(keyData.x);
         }
+
         if (isDriving)
         {
             Drive(keyData.y);
