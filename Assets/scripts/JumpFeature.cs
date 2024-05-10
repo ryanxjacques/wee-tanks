68b894f8141c2b08c2f0a5e4ca3e90eec3fa3d45
Joseph Erlinger <josepherlinger@dyn-10-109-37-193.wless.uoregon.edu>
2024-05-10 14:09:27 -0700

    Refactor some of 'JumpFeature.cs'

--- Assets/scripts/JumpFeature.cs
+++ Assets/scripts/JumpFeature.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// THIS COMMENT IS OUT OF DATE!!
 /* Comment's Date: 30th April 2024
  * The JumpFeature component defines the player's jump.
  * Usuage: Call DrawProjection to display the entity's jump trajectory.
@@ -21,80 +22,107 @@ public interface IJumpable
 }
 
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(LineRenderer))]
 public class JumpFeature : MonoBehaviour
 {
+    /* Configurable Variables (Configure via editor) */
     [Header("Assets")]
-    [SerializeField]
-    private LineRenderer LineRenderer;
-    [SerializeField]
-    private GameObject JumpReticleAsset;
-    [Header("Display Controls")]
-    [SerializeField] 
-    [Range(10, 100)] 
+    [SerializeField] private GameObject JumpReticleAsset;
+    
+    [Header("Trajectory Configuration")]
+    [SerializeField] [Range(10, 100)] 
     private int LinePoints = 25;
-    [SerializeField]
-    [Range(0.01f, 0.25f)]
-    private float TimeBetweenPoints = 0.1f;
-    [SerializeField]
-    private float minDist = 0f;
-    [SerializeField]
-    private float maxDist = 20.0f;
-    [SerializeField]
-    private float minRadius = 1f;
-    [SerializeField]
-    private float maxRadius = 2.0f;
+    [SerializeField] [Range(0.01f, 0.25f)] 
+    private float TimeBetweenPoints = 0.1f; 
+    
+    [Header("Jump Controls")]
+    [SerializeField] private float minDist;
+    [SerializeField] private float maxDist;
+    
+    [Header("Impact Zone Controls")]
+    [SerializeField] private float minRadius;
+    [SerializeField] private float maxRadius;
 
-
-    // Do not modify.
+    /* Instance Variables */
+    private Rigidbody rigidbody;
+    private LineRenderer lineRenderer;
     private GameObject JumpReticle;
-    private float jumpGrowthRate;
-    private float durationTime = 0;
-    private float _velocity_;
-    private Rigidbody _rigidbody;
-    private object _subject;
     private LayerMask JumpFeatureCollisionMask;
     private int jumpFeatureLayer;
+    private float jumpGrowthRate;
+    private float durationTime;
+    private int maxPoints;
+    private float _velocity_;
+    private object subject;
 
-
+    // Start is called before the first frame.
     private void Start()
     {
-        _rigidbody = GetComponent<Rigidbody>();
-        JumpReticle = Instantiate(JumpReticleAsset, new Vector3(0, 0, 0), Quaternion.identity);
-        JumpReticle.SetActive(false); // stop Updating the JumpReticle (i.e. rendering it).
+        durationTime = 0;
+        rigidbody = GetComponent<Rigidbody>();
+        lineRenderer = GetComponent<LineRenderer>();
+        maxPoints = Mathf.CeilToInt(LinePoints / TimeBetweenPoints) + 1;
+        JumpReticle = Instantiate(JumpReticleAsset, 
+                                  new Vector3(0, 0, 0), 
+                                  Quaternion.identity);
+        JumpReticle.SetActive(false);
     }
 
-    public void InitializeJumpFeature<T>(T subject) where T : IJumpable, IStateful
+    // Call by the script you wish to make jumpable. 
+    // (i.e. the PlayerController)
+    public void InitializeJumpFeature<T>(T subject) 
+    where T : IJumpable, IStateful
     {
         subject.onPlanning += OnPlanningObserver;
         subject.onJumping += OnJumpingObserver;
         subject.onGround += OnGroundObserver;
-        this._subject = subject;
-        this.jumpGrowthRate = subject.GetSpeed() / ((maxDist - minDist) * 50);  // Speed over Distance * FrameRate.
+        this.subject = subject;
+        jumpGrowthRate = subject.GetSpeed() / ((maxDist - minDist) * 50);
         jumpFeatureLayer = this.gameObject.layer;
         CreateCollisionMask();
     }
 
+    // Growth Rate should be the same speed as the entity to prevent jumping
+    // from being faster than regular moving.
+    private float CalculateGrowthRate(float entitySpeed)
+    {
+        int FRAMERATE = 50;  // Assuming a 50 Fps.
+        return entitySpeed / ((this.maxDist - this.minDist) * FRAMERATE);
+    }
+
+    // A collision mask is a 32-bit string where each bit represents a layer.
+    // If the ith bit is 1, then that layer does not collide with the subject.
+    // Otherwise, if the ith bit is 0, then that layer is collidable.
     private void CreateCollisionMask()
     {
-        for (int i = 0; i < 32; i++) // 32 possible layers because an integer has 32 bits. 
+        // 32 possible layers because an 'int' type has 32 bits. 
+        for (int i = 0; i < 32; i++) 
         {
             if (!Physics.GetIgnoreLayerCollision(jumpFeatureLayer, i))
             {
+                // Note: These are bitwise operators.
                 int currentLayer = 1 << i;
                 JumpFeatureCollisionMask |= currentLayer;
             }
         }
     }
 
+    // Important: Subject should Invoke OnPlanning Action in FixedUpdate() 
+    // so that the framerate is constant 50 Fps.
     private void OnPlanningObserver()
     {
-        if ((_subject as IStateful).CheckState(State.OnGround))
+        // Must check if the subject is Stateful, 
+        // otherwise the compiler will complain.
+        if ((subject as IStateful).CheckState(State.OnGround))
         {
-            LineRenderer.enabled = true;
-            JumpReticle.SetActive(true); 
-            LineRenderer.positionCount = Mathf.CeilToInt(LinePoints / TimeBetweenPoints) + 1;
-            _velocity_ = LerpJumpDistance();
+            lineRenderer.enabled = true;
+            lineRenderer.positionCount = maxPoints;
+
+            JumpReticle.SetActive(true);
+/* -------------------------- Have not Refactored Code Past this point ------------------------- */
             JumpReticle.transform.localScale = new Vector3(1 * LerpJumpReticle(), 0, 1 * LerpJumpReticle());
+            _velocity_ = LerpJumpDistance();
+            
             incrementDuration();
             DrawProjection(_velocity_);
         }
@@ -103,25 +131,20 @@ public class JumpFeature : MonoBehaviour
     private void OnJumpingObserver()
     {
         Jump(_velocity_);
-        (_subject as IStateful)?.SetState(State.OnGround, false);
+        (subject as IStateful)?.SetState(State.OnGround, false);
     }
 
     private void OnGroundObserver()
     {
         durationTime = 0;
         JumpReticle.SetActive(false); 
-        LineRenderer.enabled = false;
+        lineRenderer.enabled = false;
     }
 
+    // Increment value until it reaches the max.
     private void incrementDuration()
     {
-        if (durationTime == 1f)
-            return;
-        if (durationTime > 1f) {
-            durationTime = 1f;
-            return;
-        }
-        durationTime += jumpGrowthRate;
+        durationTime = Math.Min(durationTime + jumpGrowthRate, 1f);
     }
 
     // TODO: Combine these two functions.
@@ -141,16 +164,16 @@ public class JumpFeature : MonoBehaviour
         Vector3 startPosition = transform.position;
         Vector3 startVelocity = new Vector3(direction.x * speed, 10.0f, direction.z * speed);
         int i = 0;
-        LineRenderer.SetPosition(i, startPosition);
+        lineRenderer.SetPosition(i, startPosition);
         for (float time = 0; time < LinePoints; time += TimeBetweenPoints)
         {
             i++;
             Vector3 point = startPosition + time * startVelocity;
             point.y = startPosition.y + startVelocity.y * time + (Physics.gravity.y / 2f * time * time);
-            LineRenderer.SetPosition(i, point);
+            lineRenderer.SetPosition(i, point);
             
             // RayCasting Stuff (Messy Code for now while I'm learning.)
-            Vector3 lastPosition = LineRenderer.GetPosition(i-1);
+            Vector3 lastPosition = lineRenderer.GetPosition(i-1);
             Vector3 origin = lastPosition;
             Vector3 rayDirection = (point-lastPosition).normalized;
             RaycastHit hit;
@@ -163,9 +186,9 @@ public class JumpFeature : MonoBehaviour
                             JumpFeatureCollisionMask))
             {
                 // hit is a struct that contains information about the Raycast.
-                LineRenderer.SetPosition(i, hit.point); // Move the last point to exactly where the raycast hit.
+                lineRenderer.SetPosition(i, hit.point); // Move the last point to exactly where the raycast hit.
                 JumpReticle.transform.position = new Vector3(hit.point.x, hit.point.y + 0.01f, hit.point.z);
-                LineRenderer.positionCount = i + 1; // Stop rendering more points by setting the limit of PositionCount.
+                lineRenderer.positionCount = i + 1; // Stop rendering more points by setting the limit of PositionCount.
                 return;
             }
         }
@@ -175,12 +198,12 @@ public class JumpFeature : MonoBehaviour
     {
         // BUG FIX: Tank flips after jumping.
         // Reset angular velocity and (x, z) rotation. This keeps the tank parallel to the ground.
-        _rigidbody.angularVelocity = Vector3.zero;
+        rigidbody.angularVelocity = Vector3.zero;
         transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
         
         // Now that the rotation of the tank is corrected, carry on.
         Vector3 direction = transform.rotation * new Vector3(-1, 0, 0);
-        _rigidbody.velocity = new Vector3(direction.x * speed, 10f, direction.z * speed);
+        rigidbody.velocity = new Vector3(direction.x * speed, 10f, direction.z * speed);
     }
 }
     
