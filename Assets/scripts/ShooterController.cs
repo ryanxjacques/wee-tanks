d9ce2793e999df8f3457318627bec45da13d5132
Joseph Erlinger <josepherlinger@dyn-10-109-37-193.wless.uoregon.edu>
2024-05-28 21:19:24 -0700

    Add pathfinding AI to scene 'Joseph Test'

--- Assets/scripts/ShooterController.cs
+++ Assets/scripts/ShooterController.cs
@@ -3,63 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+// Incredibly Dead-Simple AI using NavMeshAgent.
 [RequireComponent(typeof(NavMeshAgent))]
-public class ShooterController : TankParent
+public class ShooterController : MonoBehaviour
 {
     private NavMeshAgent agent;
-    private Direction direction = new Direction();
-    // private Direction agentDirection = new Direction();
-    private Vector3 agentForward;
-    // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        // Disable NavMeshAgent auto-movement
-        agent.updatePosition = false;
-        agent.updateRotation = false;
         agent.SetDestination(new Vector3(-49, 0, 0));
     }
-
-    void Update()
-    {
-        if (agent.pathPending || agent.remainingDistance > 0.1)
-        {
-            agentForward = agent.velocity.normalized;
-            if (this.forward.x - 0.01f > agent.velocity.normalized.x)
-            {
-                SetState(State.IsRotating, true);
-                direction.rotate = -1;
-            } else if (this.forward.x + 0.01f < agent.velocity.normalized.x)
-            {
-                SetState(State.IsRotating, true);
-                direction.rotate = 1;
-            } else {
-                SetState(State.IsRotating, false);
-            }
-            if (CheckState(State.IsRotating) == false)
-            {
-                SetState(State.IsDriving, true);
-            }
-        }
-        else
-        {
-            // Stop the agent when it reaches the destination
-            agent.isStopped = true;
-            SetState(State.IsRotating, false);
-            SetState(State.IsDriving, false);
-        }
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        if (CheckState(State.IsRotating))
-        {
-            Rotate(direction.rotate, agentForward);
-        }
-        if (CheckState(State.IsDriving))
-        {
-            Drive(1);
-        }
-    }
+    void Update(){}
+    void FixedUpdate(){}
 }
--- Assets/scripts/TankParent.cs
+++ Assets/scripts/TankParent.cs
@@ -13,7 +13,10 @@ using System;
  */
 public class TankParent : Entity
 {
+    [SerializeField]
     protected Vector3 forward = new Vector3 (1,0,0); // Do not change! If you do, change all lines that calculate forward.
+    [SerializeField]
+    protected Quaternion rotation;
     [Header("Tank Movement")]
     [SerializeField] protected float speed = 1f;
     [SerializeField] protected float rotation_speed = 10f;
@@ -40,16 +43,6 @@ public class TankParent : Entity
             RotateRight();
         }
     }
-    
-    // Used by AI
-    protected void Rotate(float direction, Vector3 bound)
-    {
-        if (direction == -1) {  // A is pressed
-            RotateLeft(bound);
-        } else if (direction == 1) { // D is pressed
-            RotateRight(bound);
-        }
-    }
 
     protected void DriveForward()
     {
@@ -71,35 +64,5 @@ public class TankParent : Entity
     {
         transform.rotation = Quaternion.AngleAxis(rotation_speed, Vector3.up) * transform.rotation;
         forward = transform.rotation * new Vector3(-1, 0, 0);
-    }
-
-    // Used by AI
-    protected void RotateLeft(Vector3 bound)
-    {
-        Quaternion targetRotation = Quaternion.AngleAxis(-rotation_speed, Vector3.up) * transform.rotation;
-        Vector3 targetForward = targetRotation * new Vector3(-1, 0, 0);
-        if (targetForward.x < bound.x) {
-            transform.rotation = targetRotation;
-            forward = bound;
-        } else if (targetForward.x > bound.x) {
-            transform.rotation = targetRotation;
-            forward = transform.rotation * new Vector3(-1, 0, 0);
-        }
-        Debug.Log($"LEFT {forward} {bound}");
-    }
-
-    // Used by AI
-    protected void RotateRight(Vector3 bound)
-    {
-        Quaternion targetRotation = Quaternion.AngleAxis(rotation_speed, Vector3.up) * transform.rotation;
-        Vector3 targetForward = targetRotation * new Vector3(-1, 0, 0);
-        if (targetForward.x > bound.x) {
-            transform.rotation = targetRotation;
-            forward = bound;
-        } else if (targetForward.x < bound.x) {
-            transform.rotation = targetRotation;
-            forward = transform.rotation * new Vector3(-1, 0, 0);
-        }
-        Debug.Log($"RIGHT {forward} {bound}");
     }
 }
